205dda837f7539c28b80b72e1f7966f988fa4c06
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-04-22 08:49:17 +0200

    Fix

--- src/monitor.py
+++ src/monitor.py
@@ -22,7 +22,7 @@ def refresh_ips():
 		cur.execute('SELECT ip, mail FROM ips')
 		data = cur.fetchall()
 	for row in data:
-		if row[0] in self.ips:
+		if row[0] in ips:
 			ips[row[0]].append(row[1])
 		else:
 			ips[row[0]] = [row[1]]
