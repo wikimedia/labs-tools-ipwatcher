2125536471d31231e660d7ce491d648476bc9280
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-18 20:16:50 +0200

    Let's test with kubernetes

---
+++ src/etc/ipwatcher.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+source /data/project/ipwatcher/www/python/venv/bin/activate
+
+_get_pod() {
+    kubectl get pods \
+        --output=jsonpath={.items..metadata.name} \
+        --selector=name=ipwatcher.bot
+}
+
+case "$1" in
+    start)
+        echo "Starting ipwatcher"
+        kubectl create -f /data/project/ipwatcher/etc/deployment
+        ;;
+    run)
+        date +%Y-%m-%dT%H:%M:%S
+        echo "Running ipwatcher"
+        cd /data/project/ipwatcher
+        exec python www/python/src/monitor.py
+        ;;
+    stop)
+        echo "Stopping ipwatcher"
+        kubectl delete deployment ipwatcher.bot
+    restart)
+        echo "Restarting ipwatcher"
+        $0 stop &&
+        $0 start
+        ;;
+    status)
+        echo "Active pods:"
+        kubectl get pods -l name=ipwatcher.bot
+    tail)
+        exec kubectl logs -f $(_get_pod)
+        ;;
+    attach)
+        echo "Attaching"
+        exec kubectl exec -i -t $(_get_pod) /bin/bash
+esac
+exit 0
