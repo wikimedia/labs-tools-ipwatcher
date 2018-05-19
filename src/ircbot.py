3b9245a87dcc1087609f8ad5e63d6eee29b93ed1
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-19 15:23:11 +0200

    Add ircbot
    Very basic IRC watching bot that is supposed to stay in
    all needed IRC channels in all servers and relay everything
    that should be relayed

---
+++ src/ircbot.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+##########################################################################
+# This script is supposed to stay active, watch IRC queue in the DB
+# and relay everything it founds in that queue.
+# Author: Martin Urbanec <martin.urbanec@wikimedia.cz>
+##########################################################################
+
+import socket
+import yaml
+import pymysql
+import os
+import logging
+
+def getconfig():
+	logging.info("I'm loading the config")
+	return yaml.load(open('/data/project/ipwatcher/www/python/src/config.yml'))
+
+def connect():
+	config = getconfig()
+	logging.info("I'm connecting to the local database")
+	return pymysql.connect(
+		database=config['DB_NAME'],
+		host='tools-db',
+		read_default_file=os.path.expanduser("~/replica.my.cnf"),
+		charset='utf8mb4',
+	)
+
+
+if __name__ == "__main__":
+    # Set up logging
+    #logging.basicConfig(filename='/data/project/ipwatcher/logs/ircbot.log', level=logging.DEBUG, format='%(asctime)s %(levelname)s:%(message)s')
+
+    # Required variables
+    ping = 'PING '
+    pong = 'PONG '
+
+    try:
+        # Fetch config
+        config = getconfig()
+
+        # Connect.
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(("irc.rcm.wmflabs.org", 6667))
+
+        # Set nickname, authenticate.
+        client.send('NICK IPWatcher\r\n')
+        client.send('USER IPWatcher 0 * :IPWatcher\r\n')
+        data = client.recv(1024)
+
+        client.send('PRIVMSG NickServ :IDENTIFY pasword\r\n')
+        data = client.recv(1024)
+
+        # Join initial channel
+        client.send('JOIN #Urbanecm\r\n')
+
+        # Output and ping/pong.
+        while True:
+            #client.send('PRIVMSG #Urbanecm :Testing\r\n')
+            #data = client.recv(1024)
+
+            if data.startswith(ping):
+                resp = data.strip(ping)
+                client.send(pong + resp)
+    except Exception as e:
+        logging.exception("Unknown exception occured while running")
