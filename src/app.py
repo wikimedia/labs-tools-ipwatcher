7d024daa5a99cc5e4f631b772dbf04d5de03e5ea
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-05-19 10:37:26 +0200

    Fix IRC-related column names in code

--- src/app.py
+++ src/app.py
@@ -63,7 +63,7 @@ def index():
 			ips = []
 			conn = connect()
 			with conn.cursor() as cur:
-				sql = 'SELECT ip, notify_via_mail, ircserver, ircchans FROM ips WHERE username=%s'
+				sql = 'SELECT ip, notify_via_mail, ircserver, ircchannels FROM ips WHERE username=%s'
 				cur.execute(sql, (getusername()))
 				data = cur.fetchall()
 			for row in data:
@@ -73,7 +73,7 @@ def index():
 					"ip": row[0],
 					"notify_via_mail": notify_via_mail,
 					"ircserver": row[2],
-					"ircchans": row[2]
+					"ircchannels": row[2]
 				})
 			return render_template('tool.html', logged=logged(), username=getusername(), ips=ips)
 	else:
